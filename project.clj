e69299eb6430b7a6bcec5f05508bc8c167bed18d
Korny Sietsma <korny@sietsma.com>
2018-03-27 10:56:36 +0100

    updated library versions

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject csvmerge2flare "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject csvmerge2flare "0.1.0"
+  :description "utility to merge csv files into a d3 flare data structure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-yaml "0.4.0"]
-                 [org.clojure/tools.cli "0.3.3"]
-                 [org.clojure/data.csv "0.1.3"]
-                 [semantic-csv "0.1.0"]
-                 [cheshire "5.5.0"]]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/data.csv "0.1.4"]
+                 [semantic-csv "0.2.0"]
+                 [cheshire "5.8.0"]]
   :main csvmerge2flare.cli
   :uberjar-name "csvmerge2flare.jar"
   :profiles {
              :uberjar {:aot :all}
-             :dev     {:dependencies [[midje "1.8.3"]]}
+             :dev     {:dependencies [[midje "1.9.1"]]}
              }
   :plugins [[lein-midje "3.2"]]
   )
